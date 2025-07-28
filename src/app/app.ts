64a9e2e9eee4ceb45b9d7fbdbac88de8aeab2947
Kurt Joshua Cayaga <kurtjoshuacayaga@gmail.com>
2025-07-28 19:45:26 +0800

    Update test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { First } from './first/first';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, First],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
